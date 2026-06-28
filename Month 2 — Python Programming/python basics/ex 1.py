ad0410724f4c39b8cac3762067f6a170cbef551a
DigheSakshi2007 <sakshidighe31@gmail.com>
2026-06-28 18:03:06 +0530

    Create ex 1.py

---
+++ Month 2 — Python Programming/python basics/ex 1.py
@@ -0,0 +1,68 @@
+print("hello")
+hello
+print("hello word")
+hello word
+print("welcome")
+welcome
+print("adarsh")
+adarsh
+# this is my first program
+print("hello")
+hello
+a=10
+b=20
+c=a*b
+# calculate total salary
+salary=50000
+bouns=10000
+print(salary+bouns)
+60000
+name="adarsh"
+age=20
+salary=25000
+print("adarsh")
+print("adarsh")
+print("adarsh")
+name
+name
+name
+x=y=z=100
+x=10
+print(type(x))
+<class 'int'>
+x="python"
+print(type(x))
+<class 'str'>
+name="adarsh"
+city="pune"
+age=21
+print(name)
+print(city)
+print(age)
+adarsh
+pune
+21
+student_name="rahul"
+roll_no=15
+marks=92
+college="abc college"
+print(student_name)
+print(roll_no)
+print(marks)
+print(college)
+rahul
+15
+92
+abc college
+customer="amit"
+balance=50000
+interest=4.5
+print(customer)
+print(balance)
+print(interest)
+amit
+50000
+4.5
+name="rahul"
+print(name)
+rahul
