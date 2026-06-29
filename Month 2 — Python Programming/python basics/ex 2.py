b26ef38aaf01cfb1823ef889c18be080fd6e8eb5
DigheSakshi2007 <sakshidighe31@gmail.com>
2026-06-29 16:03:47 +0530

    Add program to swap two numbers
    This program swaps two numbers using a temporary variable and displays the results before and after swapping.

---
+++ Month 2 — Python Programming/python basics/ex 2.py
@@ -0,0 +1,17 @@
+# Program to swap two numbers using a temporary variable
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("\nBefore Swapping:")
+print("First Number =", num1)
+print("Second Number =", num2)
+
+# Swapping
+temp = num1
+num1 = num2
+num2 = temp
+
+print("\nAfter Swapping:")
+print("First Number =", num1)
+print("Second Number =", num2)
