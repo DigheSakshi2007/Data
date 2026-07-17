0bc14e0fea0602518aed668b2ccd2fa2fe260865
DigheSakshi2007 <sakshidighe31@gmail.com>
2026-07-17 17:56:03 +0530

    Create ex 3.py

---
+++ Month 2 — Python Programming/variables/ex 3.py
@@ -0,0 +1,27 @@
+name="rahul" 
+roll_no=12
+course="bsc"
+marks=89
+
+print("name: ",name)
+print("roll no: ",roll_no)
+print("course: ",course)
+print("marks: ",marks)
+
+employee_name="amit"
+basic_salary=40000
+bouns=5000
+total_salary=basic_salary+bouns
+print("employee: ",employee_name)
+print("total_salary: ",total_salary)
+
+product="laptop" 
+price=55000
+quantity=2
+total=price+quantity
+print("product: ",product)
+print("total bill: ",total)
+
+a=12
+b=23
+print(a==b)
