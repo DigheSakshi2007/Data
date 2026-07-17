bf8dd43249425587a432612468b6dd0f4dcad985
DigheSakshi2007 <sakshidighe31@gmail.com>
2026-07-17 17:54:47 +0530

    Create ex 1.py

---
+++ Month 2 — Python Programming/variables/ex 1.py
@@ -0,0 +1,18 @@
+print("hello python") 
+print("sakshi")
+print("sakshi is my name","my age is 20")
+print(90)
+print(10+28)
+name="sakshi" # string
+age=20
+price=25.99
+print(name)
+print(age)
+print(price)
+print("my name is ",name)
+print("my age is",age)
+print(type(name))
+print(type(age))
+print(type(price))
+
+
